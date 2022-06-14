ce9428aba0815cd7156aadfcd125fc35e3507850
InsomniaTSO <99248369+InsomniaTSO@users.noreply.github.com>
2022-06-14 20:55:19 +0300

    Update settings.py
    Удалила djoser, в задании просят без него обойтись, типа у него избыточный функционал, нужно свой писать.

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -23,7 +23,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'djoser',
     'users.apps.UsersConfig',
     'titles.apps.TitlesConfig',
     'api.apps.APIConfig',
