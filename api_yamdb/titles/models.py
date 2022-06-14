b0d7241cd9cfeb0570babb505d6adb8515920502
Матвей Бондаренко <m.bondarenko.04@mail.ru>
2022-06-14 22:13:29 +0300

    Добавлены модели Review и Comment. Создан views.py для titles

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+from users.models import User
+
+SCORE_CHOICES = (
+    (1, 1), (2, 2),
+    (3, 3), (4, 4),
+    (5, 5), (6, 6),
+    (7, 7), (8, 8),
+    (9, 9), (10, 10)
+)
+
 
 class Category(models.Model):
     name = models.CharField(max_length=256)
@@ -25,8 +35,42 @@ class Title(models.Model):
     category = models.ForeignKey(
         Category,
         blank=True,
+        null=True,
         on_delete=models.SET_NULL
     )
 
     def __str__(self) -> str:
         return self.name[:10]
+
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    score = models.IntegerField(choices=SCORE_CHOICES)
+    pub_date = models.DateField(
+        auto_now_add=True,
+        db_index=True
+    )
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    pub_date = models.DateField(
+        auto_now_add=True,
+        db_index=True
+    )
---
+++ api_yamdb/titles/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    pass
+
+
+class CommentsViewSet(viewsets.ModelViewSet):
+    pass
