22c86d15e305d4192b62a764ec52b39c969c8b20
Матвей Бондаренко <m.bondarenko.04@mail.ru>
2022-06-16 21:14:07 +0300

    Настроены urls для Review и Comment, добавлена PageNumber пагинация

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,10 +1,25 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import UserViewSet
+from .views import CommentsViewSet, ReviewViewSet, UserViewSet
 
 user_router = DefaultRouter()
 user_router.register('users', UserViewSet, basename='user')
+
+models_router = DefaultRouter()
+models_router.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
+models_router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentsViewSet,
+    basename='comments'
+)
+
+
 urlpatterns = [
     path('v1/users', include(user_router.urls)),
+    path('v1/', include(models_router.urls)),
 ]
