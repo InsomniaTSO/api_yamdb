a02db9b5dbc177b55c05cb7e7fbf732c15d3abc9
Матвей Бондаренко <m.bondarenko.04@mail.ru>
2022-06-20 22:57:53 +0300

    доработки по пайтесту

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -82,4 +82,4 @@ class ReadOnlyForUnauthorized(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
                 or obj.author == request.user
-                or request.user.role == 'admin')
+                or request.user.role in SAFE_ROLE)
